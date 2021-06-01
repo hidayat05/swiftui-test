fc196c63cd0d6300a040260265c607f3f0ab2af5
Nur Hidayat <hidayat.speed78@gmai.com>
2021-06-01 22:12:33 +0700

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by User on 01/06/21.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
